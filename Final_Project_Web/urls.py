c8db493c18427ac3b1356ab34efe944c086561e9
Mārtiņš Dāvis Jembergs <martins.jembergs@gmail.com>
2023-07-09 18:13:14 +0200

    Initial commit.

---
+++ Final_Project_Web/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from Final_Project_Web import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('search_clip/', views.search_clip, name='search_clip'),
+    path('send_result/<str:image_name>/', views.send_result, name='send_result'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ Final_Project_Web/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+
+import requests
+import os
+
+def home(request):
+    dataset_path = os.path.join(settings.MEDIA_ROOT, 'Images')
+
+    filenames = []
+    for i, fn in enumerate(sorted(os.listdir(dataset_path))):
+        if i >= 1000:  # only take the first 100 files
+            break
+        filename = os.path.join(settings.MEDIA_URL, 'Images', fn)
+        filenames.append(filename)
+    context = {'filenames': filenames}
+    return render(request, 'home.html', context)
+
+def search_clip(request):
+    # TODO: Implement your clip search functionality here
+    return JsonResponse({})
+
+def send_result(request, image_name):
+    key_i = (image_name[-9:])[:5]
+    my_obj = {'team': "name", 'item': key_i}
+    response = requests.get(url="https://siret.ms.mff.cuni.cz/lokoc/VBSEval/EndPoint.php", params=my_obj)
+    return JsonResponse({'result': response.text})
