75a6d458779711fb8c25e2b4bc8037e07d358e85
Mārtiņš Dāvis Jembergs <accounts@martinjemberg.com>
2023-07-12 03:07:41 +0200

    Added more formatting to top line as well as included query and selected image informational notices. Added introduction to histogram, regular approach and combined approach.

--- Final_Project_Web/urls.py
+++ Final_Project_Web/urls.py
@@ -3,14 +3,14 @@ from Final_Project_Web import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import search_clip
+from .views import search_clip, combined_clip
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('search_clip', search_clip, name='search_clip'),
+    path("combined_clip", combined_clip, name="combined_clip"),
     path('send_result/<str:image_name>/', views.send_result, name='send_result'),
     path('find_similar/<str:image_id>/', views.find_similar, name='find_similar'),
-
 ]
 
 if settings.DEBUG:
--- Final_Project_Web/views.py
+++ Final_Project_Web/views.py
@@ -7,6 +7,10 @@ import clip
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
 
+import os
+import cv2
+import numpy as np
+from scipy.spatial import distance
 import requests
 import os
 dataset_path = os.path.join(settings.MEDIA_ROOT, 'Images')
@@ -72,8 +76,9 @@ def search_clip(request, shown=None, image_size=None):
         similarities.append((image_path, similarity))
 
         print("Image: ", counter)
+        # TODO counter has be 10000, but response has to be limited to 100-1000 for bandwidth reasons unless NGROK is upgraded.
         counter += 1
-        if counter >= 1000:  # Stop processing after 100 images
+        if counter >= 100:  # Stop processing after 100 images
             break
 
     # Sort the results by similarity in descending order
@@ -87,7 +92,7 @@ def search_clip(request, shown=None, image_size=None):
         similarityExcl.append("{:.3f}".format(similarity * 100) + "%")
 
     filename_similarity_zip = zip(filenames, similarityExcl)
-    context = {'filenames': filename_similarity_zip}
+    context = {'filenames': filename_similarity_zip, "query": query}
     return render(request, 'home.html', context)
 
 def send_result(request, image_name):
@@ -98,7 +103,118 @@ def send_result(request, image_name):
     # TODO Reactivate sending to endpoint.
     return 0
 
+# TODO find_similar has to be implemented properly.
 def find_similar(request, image_id, similar_images=None):
-    # Implement functionality to find similar images here.
-    # You'll need to return some kind of response, perhaps a list of similar image filenames.
+    # Load the model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load('ViT-B/32', device=device)
+
+    # Load the query image
+    query_image = preprocess(Image.open("path_to_your_query_image.jpg")).unsqueeze(0).to(device)
+
+    # Calculate the features of the query image
+    with torch.no_grad():
+        query_features = model.encode_image(query_image)
+
+    # Prepare a list to store the filenames and similarities
+    image_similarities = []
+
+    # Go through all images in the directory
+    for filename in os.listdir('path_to_your_image_directory'):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Load and preprocess the image
+            image = preprocess(Image.open(os.path.join('path_to_your_image_directory', filename))).unsqueeze(0).to(device)
+
+            # Calculate the features of the image
+            with torch.no_grad():
+                image_features = model.encode_image(image)
+
+            # Calculate the similarity between the query image and the image
+            similarity = (query_features @ image_features.T).item()
+
+            # Add the filename and similarity to the list
+            image_similarities.append((filename, similarity))
+
+    # Sort the images by similarity
+    image_similarities.sort(key=lambda x: x[1], reverse=True)
+
+    # Print the images sorted by similarity
+    for filename, similarity in image_similarities:
+        print(f'{filename}: {similarity}')
+
     return JsonResponse({'similar_images': similar_images})
+
+# TODO find_similar_histogram needs to be properly implemented, requires button in front-end as well.
+def find_similar_histogram(request):
+    # Specify the directory of your images and the path to your query image
+    directory = "path_to_your_image_directory"
+    query_image_path = "path_to_your_query_image.jpg"
+
+    # Specify the number of bins per channel for the histogram
+    bins = [8, 8, 8]
+
+    # Load the query image and compute its histogram
+    query_image = cv2.imread(query_image_path)
+    query_hist = cv2.calcHist([query_image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
+
+    # Normalize the histogram
+    query_hist = cv2.normalize(query_hist, query_hist).flatten()
+
+    # Prepare a list to store the filenames and similarities
+    image_similarities = []
+
+    # Go through all images in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Load the image
+            image = cv2.imread(os.path.join(directory, filename))
+
+            # Compute the histogram of the image
+            hist = cv2.calcHist([image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
+
+            # Normalize the histogram
+            hist = cv2.normalize(hist, hist).flatten()
+
+            # Compute the cosine similarity between the query image's histogram
+            # and this image's histogram
+            similarity = 1 - distance.cosine(query_hist, hist)
+
+            # Add the filename and similarity to the list
+            image_similarities.append((filename, similarity))
+
+    # Sort the images by similarity
+    image_similarities.sort(key=lambda x: x[1], reverse=True)
+
+    # Print the top 5 images sorted by similarity
+    for filename, similarity in image_similarities[:5]:
+        print(f'{filename}: {similarity}')
+
+def combined_clip(request):
+    print("Combined clip function called")
+    # Load the model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+
+    # Prepare the text
+    text = clip.tokenize(["A dog on a skateboard"]).to(device)
+
+    # Prepare the image
+    image = preprocess(Image.open("dog_on_skateboard.jpg")).unsqueeze(0).to(device)
+
+    # Calculate features
+    with torch.no_grad():
+        text_features = model.encode_text(text)
+        image_features = model.encode_image(image)
+
+    # Combine the features
+    combined_features = (text_features + image_features) / 2  # Here we average the features, but you can combine them in other ways
+
+    # Compare to database
+    # This depends on how your database is structured, but you would compare `combined_features` to the features of each image in the database
+    # For example, if `database_features` is a 2D tensor where each row is the features of one image:
+    similarities = torch.nn.functional.cosine_similarity(combined_features, database_features)
+
+    # Sort by similarity
+    ranked_indices = torch.argsort(similarities, descending=True)
+
+    # Now `ranked_indices[0]` is the index of the most similar image, `ranked_indices[1]` is the second most similar, and so on
